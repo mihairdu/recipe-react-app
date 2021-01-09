9c1cc149933444b1ae9e8259dc27d63dc9e8edaf
Mihai-Daniel Radu <mihaidaniel.radu00@gmail.com>
2021-01-09 16:56:26 +0200

    Basic request to food search api

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
-import React from 'react';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header>
-        <h1>Hello React</h1>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	//EDAMAM API signup id and key
+	const APP_ID = "30e006ba";
+	const APP_KEY = "31fc5934030b41b4340192e4e60b8793";
+
+	useEffect(() => {}, []);
+
+	const getRecipes = async () => {
+		const response = await fetch(
+			`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
+		);
+		const data = response.json();
+		console.log(data);
+	};
+
+	return (
+		<div className="App">
+			<form className="search-form">
+				<input className="search-bar" type="text" />
+				<button className="search-button" type="submit">
+					Search
+				</button>
+			</form>
+			<h1 onClick={() => setCounter(counter + 1)}>{counter}</h1>
+		</div>
+	);
+};
 
 export default App;
