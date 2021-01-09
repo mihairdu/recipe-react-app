feeb90664783d7622b59a54be82f2d01b3df4d00
Mihai-Daniel Radu <mihaidaniel.radu00@gmail.com>
2021-01-09 17:47:21 +0200

    Added custom css module for styling

--- src/Recipe.js
+++ src/Recipe.js
@@ -1,8 +1,9 @@
 import React from "react";
+import styles from "./recipe.module.css";
 
 const Recipe = ({ title, calories, image, ingredients }) => {
 	return (
-		<div>
+		<div className={styles.recipe}>
 			<h1>{title}</h1>
 			<ol>
 				{ingredients.map((ingredient) => (
